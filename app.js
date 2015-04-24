279ee38c6f59251ac9e399360b3b66b0f41280bc
ronfe <chihungfei@126.com>
2015-04-24 17:49:16 +0800

    modify config

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var wechat = require('wechat');
 //do not upload to github, find it on trello
-var wechatConfig = require('./secret');
+var wechatConfig = {
+  token: 'weixin',
+  appid: 'wx47201bcf25891a49',
+  encodingAESKey: 'f3a314703bf7d6a87fc26f9dbb236bff'
+};
 
 var app = express();
 app.use(express.query());
--- secret.js
+++
@@ -1,5 +0,0 @@
-module.export = {
-  token: 'weixin',
-  appid: 'wx47201bcf25891a49',
-  encodingAESKey: 'f3a314703bf7d6a87fc26f9dbb236bff'
-};
